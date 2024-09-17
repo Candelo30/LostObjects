27e37c20d4d7529be2e6fa340013053bd27c0b6a
davinchi <david.roman.ietas@gmail.com>
2024-09-17 11:51:51 -0500

    errores de registro y publicaciones solucionado

--- django/datos/settings.py
+++ django/datos/settings.py
@@ -145,8 +145,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+<<<<<<< HEAD
 STATIC_URL = '/static/'
 
+=======
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+>>>>>>> 9aa84fbcd143283cd25b23a27a8ad1423dbaecbb
 
 MEDIA_URL = '/media/'
 
